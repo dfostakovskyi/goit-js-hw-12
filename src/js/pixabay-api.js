ca0ae04992d60c67699ee7247d326fa9a6c08f95
Dfostakovskyi <d.fostakovskiy@gmail.com>
2024-11-02 09:00:37 -0700

    02.10.24.9-00

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 const API_KEY = '46850539-7e4ca2d811908faae801b6294';
 const baseURL = "https://pixabay.com/api/";
 
-export function fetchData(userInput, page = 1, per_page = 200) { // Збільшимо кількість зображень до 40
+export function fetchData(userInput, page = 1, per_page = 200) { 
   const params = new URLSearchParams({
     key: API_KEY,
     q: userInput,
